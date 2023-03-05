553fe17bd57077e2d4f77a9d3082cc5f718e0ec8
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-03-04 22:18:54 -0800

    Create menu items

---
+++ Invoicing system for a groceries shop/src/project_classes/MenuItems.java
@@ -0,0 +1,18 @@
+package project_classes;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MenuItems {
+    public static Map<Integer, String> mainMenuItems() {
+        Map<Integer, String> menuItems = new HashMap<>();
+        menuItems.put(1, "Shop Settings");
+        menuItems.put(2, "Manage Shop Items");
+        menuItems.put(3, "Create New Invoice");
+        menuItems.put(4, "Report: Statistics");
+        menuItems.put(5, "Report: All Invoices");
+        menuItems.put(6, "Search (1) Invoice");
+        menuItems.put(7, "Program Statistics");
+        menuItems.put(8, "Exit");
+        return menuItems;
+    }
+}
